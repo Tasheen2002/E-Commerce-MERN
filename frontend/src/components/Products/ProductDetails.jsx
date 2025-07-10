5d8ff3d85d35bb4b3447e1e613558e52c8a0efbc
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-10 13:47:59 +0530

    added main image manually

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-const selectedProducts={
-    name:"Stylish Jacket",
-    price:120,
-    originalPrice:150,
-    description:"This is a stylish jacket perfect for any occasion",
-    brand:"Fashion Brand",
-    material:"Leather",
-    sizes:["S","M","L","XL"],
-    colors:["Red","Black"],
-    images:[
-        {
-            url:"https://picsum.photos/500/500?random=1",
-            altText:"Stylish Jacket 1",
-        },
-        {
-            url:"https://picsum.photos/500/500?random=2",
-            altText:"Stylish Jacket 2",
-        },
-    ]
-}
+const selectedProduct = {
+  name: "Stylish Jacket",
+  price: 120,
+  originalPrice: 150,
+  description: "This is a stylish jacket perfect for any occasion",
+  brand: "Fashion Brand",
+  material: "Leather",
+  sizes: ["S", "M", "L", "XL"],
+  colors: ["Red", "Black"],
+  images: [
+    {
+      url: "https://picsum.photos/500/500?random=1",
+      altText: "Stylish Jacket 1",
+    },
+    {
+      url: "https://picsum.photos/500/500?random=2",
+      altText: "Stylish Jacket 2",
+    },
+  ],
+};
 
 const ProductDetails = () => {
   return (
@@ -27,7 +27,27 @@ const ProductDetails = () => {
       <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
         <div className="flex flex-col md:flex-row">
           {/* Left Thumbnails */}
-          <div className="hidden md:flex flex-col space-y-4 mr-6"></div>
+          <div className="hidden md:flex flex-col space-y-4 mr-6">
+            {selectedProduct.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.altText || `Thumbnail ${index}`}
+                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+              />
+            ))}
+          </div>
+          {/* Main Image */}
+            <div className="md:w-1/2">
+              <div className="mb-4">
+                <img
+                  src={selectedProduct.images[0]?.url}
+                  alt="Main Product"
+                  className="w-full h-auto object-cover rounded-lg"
+                />
+              </div>
+            </div>
+            
         </div>
       </div>
     </div>
