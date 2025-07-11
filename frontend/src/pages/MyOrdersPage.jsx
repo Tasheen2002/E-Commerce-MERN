f5d6eeaee4debbf0faaeea73ca7412aa5cf3299f
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-11 16:10:06 +0530

    created table structure

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -37,7 +37,28 @@ const MyOrdersPage = () => {
     },1000);
   },[]);
 
-  return <div>MyOrdersPage</div>;
+  return (
+    <div className='max-w-7xl mx-auto p-4 sm:p-6'>
+      <h2 className="text-xl sm:text-2xl font-bold mb-6">
+        My Orders
+      </h2>
+      <div className="relative shadow-md sm:rounded-lg overflow-hidden">
+        <table className="min-w-full text-left text-gray-500">
+          <thead className='bg-blue-100 text-xs uppercase text-gray-700'>
+            <tr>
+              <th className="py-2 px-4 sm:py-3">Image</th>
+              <th className="py-2 px-4 sm:py-3">Order ID</th>
+              <th className="py-2 px-4 sm:py-3">Created</th>
+              <th className="py-2 px-4 sm:py-3">Shipping Address</th>
+              <th className="py-2 px-4 sm:py-3">Items</th>
+              <th className="py-2 px-4 sm:py-3">Price</th>
+              <th className="py-2 px-4 sm:py-3">Status</th>
+            </tr>
+          </thead>
+          </table>
+      </div>
+    </div>
+  )
 };
 
 export default MyOrdersPage;
