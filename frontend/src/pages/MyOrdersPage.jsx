d99d95d54db915bc751b8fb09065fcf0e250249b
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-11 16:07:42 +0530

    created orders manually

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 const MyOrdersPage = () => {
-  return (
-    <div>MyOrdersPage</div>
-  )
-}
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      const mockOrders = [
+        {
+          _id: "12345",
+          createdAt: new Date(),
+          shippingAddress: { city: "New York", country: "USA" },
+          orderItems: [
+            {
+              name: "Product 1",
+              image: "https://picsum.photos/500/500/?random=1",
+            },
+          ],
+          totalPrice: 100,
+          isPaid: true,
+        },
+        {
+          _id: "34567",
+          createdAt: new Date(),
+          shippingAddress: { city: "New York", country: "USA" },
+          orderItems: [
+            {
+              name: "Product 1",
+              image: "https://picsum.photos/500/500/?random=2",
+            },
+          ],
+          totalPrice: 100,
+          isPaid: true,
+        },
+      ];
+      setOrders(mockOrders);
+    },1000);
+  },[]);
 
-export default MyOrdersPage
+  return <div>MyOrdersPage</div>;
+};
+
+export default MyOrdersPage;
