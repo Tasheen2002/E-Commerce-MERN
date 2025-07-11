122fe43a5d884d5795f14352e25bffa2d901b3c9
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-11 22:29:19 +0530

    filters added using useState

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const FilterSidebar = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [filters, setFilters] = useState({
+    category: "",
+    gender: "",
+    color: "",
+    size: [],
+    material: [],
+    brand: [],
+    minPrice: 0,
+    maxPrice: 100,
+  });
+
   return <div>FilterSidebar</div>;
 };
 
