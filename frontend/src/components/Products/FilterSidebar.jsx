9e6606f346b46699e202e94845ced1a2192c2445
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-11 23:27:04 +0530

    query parameter converted to a object and using it update filters

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -54,7 +54,30 @@ const FilterSidebar = () => {
 
   const genders = ["Men", "Women"];
 
-  return <div>FilterSidebar</div>;
+  useEffect(() => {
+    const params = Object.fromEntries([...searchParams]);
+
+    setFilters({
+      category: params.category || "",
+      gender: params.gender || "",
+      color: params.color || "",
+      size: params.size ? params.size.split(",") : [],
+      material: params.material ? params.material.split(",") : [],
+      brand: params.brand ? params.brand.split(",") : [],
+      minPrice: params.minPrice || 0,
+      maxPrice: params.maxPrice || 100,
+    });
+    setPriceRange([0, params.maxPrice || 100]);
+  }, [searchParams]);
+
+  return (
+    <div className="p-4">
+        <h3 className="text-xl font-medium text-gray-800 mb-4"></h3>
+
+        {/* Category Filter */}
+        
+    </div>
+  )
 };
 
 export default FilterSidebar;
