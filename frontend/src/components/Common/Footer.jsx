d5dc72d23b50bf5df721b5e816457865d382b260
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-11 23:47:25 +0530

    updated footer left margin

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -7,16 +7,16 @@ import { FiPhoneCall } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className="border-t py-12">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0">
+    <footer className="border-t py-12 px-10">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-10 px-4 lg:px-0">
         <div>
-          <h3 className="text-base text-gray-800 mb-4">News Letter</h3>
+          <h3 className="text-lg text-gray-800 mb-4">Newsletter</h3>
           <p className="text-gray-500 mb-4">
             Be the first to hear about new products, exclusive events, and
             online offers.
           </p>
           <p className="font-medium text-sm text-gray-600 mb-6">
-            Sign up and 10% off your first order
+            Sign up and get 10% off on your first order.
           </p>
 
           {/* Newsletter form */}
@@ -26,47 +26,57 @@ const Footer = () => {
               placeholder="Enter your email"
               className="p-3 w-full text-sm border-t border-l border-b border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all"
               required
-            ></input>
+            />
             <button
               type="submit"
-              className="bg-black text-white px-6 py-3 rounded-r-md hover:bg-gray-800 transition-all"
+              className="bg-squad-navy-blue text-white px-4 py-3 text-sm rounded-r-md hover:bg-squad-dark-light-blue transition-all cursor-pointer"
             >
               Subscribe
             </button>
           </form>
         </div>
-
         {/* Shop Links */}
         <div>
           <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
-          <ul className="space-y-2 text-gray-600">
+          <ul className="space-y-2 text-gray-600 px-3">
             <li>
-              <Link to="#" className="hover:text-gray-500 transition-colors">
+              <Link
+                to="/collections/all?gender=Men"
+                className="hover:text-gray-500 transition-colors"
+              >
                 Men's Top Wear
               </Link>
             </li>
             <li>
-              <Link to="#" className="hover:text-gray-500 transition-colors">
+              <Link
+                to="/collections/all?gender=Women"
+                className="hover:text-gray-500 transition-colors"
+              >
                 Women's Top Wear
               </Link>
             </li>
             <li>
-              <Link to="#" className="hover:text-gray-500 transition-colors">
+              <Link
+                to="/collections/all?category=Top Wear"
+                className="hover:text-gray-500 transition-colors"
+              >
                 Men's Bottom Wear
               </Link>
             </li>
             <li>
-              <Link to="#" className="hover:text-gray-500 transition-colors">
+              <Link
+                to="/collections/all?category=Bottom Wear"
+                className="hover:text-gray-500 transition-colors"
+              >
                 Women's Bottom Wear
               </Link>
             </li>
           </ul>
         </div>
-
         {/* Support Links */}
         <div>
           <h3 className="text-lg text-gray-800 mb-4">Support</h3>
-          <ul className="space-y-2 text-gray-600">
+          <ul className="space-y-2 text-gray-600 px-3">
             <li>
               <Link to="#" className="hover:text-gray-500 transition-colors">
                 Contact Us
@@ -89,50 +99,46 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-
-        {/* Follow Us */}
+        {/* Social Links */}
         <div>
           <h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
-          <div className="flex items-center space-x-4 mb-6">
+          <div className="flex items-center space-x-4 mb-6 ">
             <a
-              href="https://www.facebook.com/"
+              href="https://www.facebook.com"
               target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
+              rel="noreferrer noopener"
+              className="hover:text-gray-400"
             >
               <TbBrandMeta className="h-5 w-5" />
             </a>
-
             <a
-              href="https://www.facebook.com/"
+              href="https://www.instagram.com"
               target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
+              rel="noreferrer noopener"
+              className="hover:text-gray-400"
             >
               <IoLogoInstagram className="h-5 w-5" />
             </a>
-
             <a
-              href="https://www.facebook.com/"
+              href="https://www.x.com"
               target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
+              rel="noreferrer noopener"
+              className="hover:text-gray-400"
             >
               <RiTwitterXLine className="h-4 w-4" />
             </a>
           </div>
-          <p className="text-gray-500">Call Us</p>
+          <p className="text-gray-500 ">Call Us</p>
           <p>
             <FiPhoneCall className="inline-block mr-2" />
-            +94 766 940 529
+            0123-456-789
           </p>
         </div>
       </div>
-
-      {/* Footer Bottom */}
-      <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6">
-        <p className="text-grey-500 text-sm tracking-tighter text-center">
-          @ 2025, CompileTab. All rights reserved
+      {/* Bottom Footer */}
+      <div className=" container mx-auto border-t mt-12 px-4 lg:px-0 border-gray-300 pt-6">
+        <p className="text-gray-500 text-sm text-center tracking-tighter">
+          &copy; 2025, Squad. All Rights Reserved.
         </p>
       </div>
     </footer>
