c2865bba6b37bef7d5e6ef0ccc09d075862b110b
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-12 15:38:29 +0530

    created handle checkout function

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -2,8 +2,16 @@ import React from "react";
 // import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import CartContents from "../Cart/CartContents";
+import { useNavigate } from "react-router-dom";
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
+
+  const navigate = useNavigate();
+
+  const handleCheckout = () => {
+    toggleCartDrawer();
+    navigate("/checkout");
+  };
   return (
     <div
       className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[25rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
@@ -20,12 +28,15 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
       {/* Cart content with scrollable area */}
       <div className="flex-grow py-14 px-4 overflow-auto">
         <h2 className="text-lg font-semibold mb-4">Your Cart</h2>
-        <CartContents/>
+        <CartContents />
       </div>
 
       {/* Checkout button */}
       <div className="p-4 bg-white sticky bottom-0">
-        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+        <button
+          onClick={handleCheckout}
+          className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition"
+        >
           Checkout
         </button>
         <p className="text-xs tracking-tihter text-gray-500 mt-2 text-center">
