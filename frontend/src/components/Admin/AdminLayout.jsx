d6802d8965f08a506154de3dc4a87ab5ca84df3e
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-07 15:22:05 +0530

    Created Mobile Toggle Button

---
+++ frontend/src/components/Admin/AdminLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
+const AdminLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row relative">
+      {/* Mobile Toggle Button */}
+      <div className="flex md:hidden p-4 bg-gray-900 text-white z-20">
+        <button onClick={toggleSidebar}>
+          <FaBars size={24} />
+        </button>
+        <h1 className="ml-4 text-xl font-medium">Admin Dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
