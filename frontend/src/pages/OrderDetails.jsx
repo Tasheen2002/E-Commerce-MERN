2066e196e6fba919b3e8daaf094554dc4a4c43e7
Tasheen2002 <darshikatasheen99@gmail.com>
2025-08-07 13:56:02 +0530

    Added custometr, payment shipping info

--- frontend/src/pages/OrderDetails.jsx
+++ frontend/src/pages/OrderDetails.jsx
@@ -46,33 +46,52 @@ const OrderDetails = () => {
       ) : (
         <div className="p-4 sm:p-6 rounded-lg border">
           {/* Order info */}
-          <div>
-            <h3 className="text-lg md:text-xl font-semibold">
-              Order ID: #{orderDetails._id}
-            </h3>
-            <p className="text-gray-600">
-              {new Date(orderDetails.createdAt).toLocaleDateString()}
-            </p>
+          <div className="flex flex-col sm:flex-row justify-between mb-8">
+            <div>
+              <h3 className="text-lg md:text-xl font-semibold">
+                Order ID: #{orderDetails._id}
+              </h3>
+              <p className="text-gray-600">
+                {new Date(orderDetails.createdAt).toLocaleDateString()}
+              </p>
+            </div>
+            <div className="flex flex-col items-start sm:items-end mt-4 sm:mt-0">
+              <span
+                className={`${
+                  orderDetails.isPaid
+                    ? "bg-green-100 text-green-700"
+                    : "bg-red-100 text-red-700"
+                } px-3 py-1 rounded-full text-sm font-medium mb-2 `}
+              >
+                {orderDetails.isPaid ? "Approved" : "Pending"}
+              </span>
+              <span
+                className={`${
+                  orderDetails.isDelivered
+                    ? "bg-green-100 text-green-700"
+                    : "bg-yellow-100 text-yellow-700"
+                } px-3 py-1 rounded-full text-sm font-medium mb-2 `}
+              >
+                {orderDetails.isDelivered ? "Delivered" : "Pending Delivery"}
+              </span>
+            </div>
           </div>
-          <div className="flex flex-col items-start sm:items-end mt-4 sm:mt-0">
-            <span
-              className={`${
-                orderDetails.isPaid
-                  ? "bg-green-100 text-green-700"
-                  : "bg-red-100 text-red-700"
-              } px-3 py-1 rounded-full text-sm font-medium mb-2 `}
-            >
-              {orderDetails.isPaid ? "Approved" : "Pending"}
-            </span>
-            <span
-              className={`${
-                orderDetails.isDelivered
-                  ? "bg-green-100 text-green-700"
-                  : "bg-yellow-100 text-yellow-700"
-              } px-3 py-1 rounded-full text-sm font-medium mb-2 `}
-            >
-              {orderDetails.isDelivered ? "Delivered" : "Pending Delivery"}
-            </span>
+
+          {/* Customer, Payment, Shipping Info */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-8">
+            <div>
+              <h4 className="text-lg font-semibold mb-2">Payment Info</h4>
+              <p>Payment Method: {orderDetails.paymentMethod}</p>
+              <p>Status: {orderDetails.isPaid ? "Paid" : "Unpaid"}</p>
+            </div>
+            <div>
+              <h4 className="text-lg font-semibold mb-2">Shipping Info</h4>
+              <p>Shipping Method: {orderDetails.shippingMethod}</p>
+              <p>
+                Address:{" "}
+                {`${orderDetails.shippingAddress.city}, ${orderDetails.shippingAddress.country}`}
+              </p>
+            </div>
           </div>
         </div>
       )}
